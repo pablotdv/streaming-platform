237b91d150da6c616b0ed15e3e80e3d336e7cf66
Pablo T. de Vargas <pablotdvsm@gmail.com>
2023-09-19 10:48:34 -0300

    add oidc

--- go.mod
+++ go.mod
@@ -3,14 +3,20 @@ module github.com/pablotdv/streaming-platform
 go 1.20
 
 require (
+	github.com/coreos/go-oidc v2.2.1+incompatible
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
 )
 
 require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/pquerna/cachecontrol v0.2.0 // indirect
+	golang.org/x/oauth2 v0.12.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
 
 require (
@@ -33,11 +39,11 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.1
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/coreos/go-oidc"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/pablotdv/streaming-platform/data"
@@ -19,6 +24,53 @@ func init() {
 	data.Db.AutoMigrate(&models.Streamer{})
 }
 
+var provider *oidc.Provider
+var verifier *oidc.IDTokenVerifier
+
+func init() {
+	ctx := context.Background()
+
+	// Inicialize o provider OIDC usando o endpoint de descoberta do IdentityServer.
+	var err error
+	provider, err = oidc.NewProvider(ctx, "https://localhost:5001")
+	if err != nil {
+		log.Fatalf("Failed to get provider: %v", err)
+	}
+
+	// Configure um OIDC Config para o token.
+	oidcConfig := &oidc.Config{
+		ClientID: "streaming-api", // Substitua com o ID do cliente registrado no IdentityServer.
+	}
+
+	// Crie um verificador para tokens.
+	verifier = provider.Verifier(oidcConfig)
+}
+
+func OIDCTokenValidation() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		rawToken := c.Request.Header.Get("Authorization")
+		if rawToken == "" {
+			c.JSON(http.StatusUnauthorized, "Authorization header missing")
+			c.Abort()
+			return
+		}
+
+		// Remova o prefixo "Bearer" se estiver presente.
+		if len(rawToken) > 7 && rawToken[0:7] == "Bearer " {
+			rawToken = rawToken[7:]
+		}
+
+		_, err := verifier.Verify(c, rawToken)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, err.Error())
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
@@ -29,6 +81,7 @@ func main() {
 	}))
 	api := router.Group("/api")
 	{
+		api.Use(OIDCTokenValidation())
 		streamers := api.Group("/streamers")
 		{
 			streamers.GET("", routes.GetStreamers)
